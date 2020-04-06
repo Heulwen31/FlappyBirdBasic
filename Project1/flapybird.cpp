006bb3d531f0bf581aebb5d9480cde10d5205f34
= <=>
2020-04-06 16:33:52 +0700

    Load Background

--- Project1/flapybird.cpp
+++ Project1/flapybird.cpp
@@ -1,7 +1,171 @@
+//Using SDL, standard IO, and strings
 #include <SDL.h>
+#include <iostream>
+#include <SDL_image.h>
 using namespace std;
 
-int main(int argc, char* argv[])
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+//Starts up SDL and creates window
+bool init();
+
+//Loads media
+bool loadMedia();
+
+//Frees media and shuts down SDL
+void close();
+
+//Loads individual image
+SDL_Surface* loadSurface(std::string path);
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+
+//Current displayed image
+SDL_Surface* gStretchedSurface = NULL;
+
+
+
+int main(int argc, char* args[])
 {
+	//Start up SDL and create window
+	if (!init())
+	{
+		cout << "ERROR\n";
+	}
+	else
+	{
+		//Load media
+		if (!loadMedia())
+		{
+			cout << "ERROR\n";
+		}
+		else
+		{
+			//Main loop flag
+			bool quit = false;
+
+			//Event handler
+			SDL_Event e;
+
+			//While application is running
+			while (!quit)
+			{
+				//Handle events on queue
+				while (SDL_PollEvent(&e) != 0)
+				{
+					//User requests quit
+					if (e.type == SDL_QUIT)
+					{
+						quit = true;
+					}
+				}
+
+				//Apply the image stretched
+				SDL_Rect stretchRect;
+				stretchRect.x = 0;
+				stretchRect.y = 0;
+				stretchRect.w = SCREEN_WIDTH;
+				stretchRect.h = SCREEN_HEIGHT;
+				SDL_BlitScaled(gStretchedSurface, NULL, gScreenSurface, &stretchRect);
+
+				//Update the surface
+				SDL_UpdateWindowSurface(gWindow);
+			}
+		}
+	}
+
+	//Free resources and close SDL
+	close();
+
 	return 0;
+}
+
+bool init()
+{
+	//Initialization flag
+	bool success = true;
+
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		success = false;
+	}
+	else
+	{
+		//Create window
+		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
+		{
+			success = false;
+		}
+		else
+		{
+			//Get window surface
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
+		}
+	}
+
+	return success;
+}
+
+bool loadMedia()
+{
+	//Loading success flag
+	bool success = true;
+
+	//Load stretching surface
+	gStretchedSurface = loadSurface("image/bg.png");
+	if (gStretchedSurface == NULL)
+	{
+		success = false;
+	}
+
+	return success;
+}
+
+void close()
+{
+	//Free loaded image
+	SDL_FreeSurface(gStretchedSurface);
+	gStretchedSurface = NULL;
+
+	//Destroy window
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
+}
+
+SDL_Surface* loadSurface(std::string path)
+{
+	//The final optimized image
+	SDL_Surface* optimizedSurface = NULL;
+
+	//Load image at specified path
+	SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+	if (loadedSurface == NULL)
+	{
+		cout << "ERROR 1\n";
+	}
+	else
+	{
+		//Convert surface to screen format
+		optimizedSurface = SDL_ConvertSurface(loadedSurface, gScreenSurface->format, 0);
+		if (optimizedSurface == NULL)
+		{
+			cout << "ERROR 2\n";
+		}
+
+		//Get rid of old loaded surface
+		SDL_FreeSurface(loadedSurface);
+	}
+
+	return optimizedSurface;
 }
